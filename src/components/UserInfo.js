97aff95d40e80dbee5a27b52b073d09a073b4835
Pinar Gulum <pinargulum@hotmail.com>
2024-04-29 11:05:08 -0400

    update

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -6,17 +6,17 @@ export default class UserInfo {
     this._jobElement = document.querySelector(jobSelector);
   }
 
-  getUserInfo() {
-  title.value = this._nameElement.textContent;
-    description.value = this._jobElement.textContent;
-     this._nameElement.title = this._nameElement.value;
-    this._jobElement.description = this._jobElement.value;
-    
-  }
+  
+    getUserInfo() {
+      this._nameElement.value = this._nameElement.textContent;
+      this._jobElement.value = this._jobElement.textContent;
+      
+    }
+  
 
   setUserInfo({ title, description }) {
-    //this._nameElement.title = this._nameElement.value;
-    //this._jobElement.description = this._jobElement.value;
+    this._nameElement.title = this._nameElement.value;
+    this._jobElement.description = this._jobElement.value;
     this._nameElement.textContent = title;
     this._jobElement.textContent = description;
   }
