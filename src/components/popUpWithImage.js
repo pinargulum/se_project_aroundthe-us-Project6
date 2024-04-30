e8e7c5fdd362ce9d32d456b9c02714838792bdcb
Pinar Gulum <pinargulum@hotmail.com>
2024-04-30 02:57:03 -0400

    update

--- src/components/popUpWithImage.js
+++ src/components/popUpWithImage.js
@@ -8,7 +8,7 @@ export default class PopupWithImage extends Popup {
 
   open(data) {
     this.modalImage.src = data._link;
-    this.imageText.alt = data._name; //there is "alt" here. and it does show it on new cards and image preview.
+    this.modalImage.alt = data._name;
     this.imageText.textContent = data._name;
     super.open();
   }
