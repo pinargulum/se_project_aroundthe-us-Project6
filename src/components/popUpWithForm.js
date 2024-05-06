a6f0a87454335d0872d8d7f034fafdab77e4be61
Pinar Gulum <pinargulum@hotmail.com>
2024-05-06 10:28:17 -0400

    Update popUpWithForm.js

--- src/components/popUpWithForm.js
+++ src/components/popUpWithForm.js
@@ -1,24 +1,24 @@
 import PopUp from "./Popup.js";
 
-export default class PopupWithForm extends PopUp {
+export default class PopupWithForm extends Popup {
   constructor(popUpSelector, handleFormSubmit) {
-    super(popUpSelector);
-    this._popUpForm = this._popUpElement.querySelector(".modal__form");
+    super(popupSelector);
+    this._popupForm = this._popupElement.querySelector(".modal__form");
     this._handleFormSubmit = handleFormSubmit;
   }
  
   setEventListeners() {
     super.setEventListeners();
-    this._popUpForm.addEventListener("submit", (evt) => {
+    this._popupForm.addEventListener("submit", (evt) => {
       evt.preventDefault();
       const inputValues = this._getInputValues();
       this._handleFormSubmit(inputValues);
-      this._popUpForm.reset();
+      this._popupForm.reset();
     });
   }
   _getInputValues() {
     const inputValues = {};
-    const inputValueList = this._popUpForm.querySelectorAll(".modal__input");
+    const inputValueList = this._popupForm.querySelectorAll(".modal__input");
     inputValueList.forEach((input) => {
       inputValues[input.name] = input.value;
     });
